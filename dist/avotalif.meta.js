403a6ac08e7c1d25faca29b1a74882803d954628
Salaron <breykin.alexey@gmail.com>
2021-04-24 01:04:09 +0500

    Add builded userscript to the repo

---
+++ dist/avotalif.meta.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @run-at       document-start
+// @name         avotalif
+// @description  Yet another script for VK.
+// @author       Salaron
+// @version      1.0.0
+// @match        *://*.vk.com/*
+// @require      https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js
+// @grant        GM_addStyle
+// @grant        GM_getValue
+// @grant        GM_setValue
+// @grant        GM_xmlhttpRequest
+// @noframes
+// ==/UserScript==
--- webpack.config.ts
+++ webpack.config.ts
@@ -1,11 +1,30 @@
-import { Configuration } from "webpack"
+import { Configuration, BannerPlugin } from "webpack"
 import * as path from "path"
+import * as fs from "fs"
+import pkg from "./package.json"
+
+const scriptHeader = `\
+// ==UserScript==
+// @run-at       document-start
+// @name         ${pkg.name}
+// @description  ${pkg.description}
+// @author       ${pkg.author}
+// @version      ${pkg.version}
+// @match        *://*.vk.com/*
+// @require      https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js
+// @grant        GM_addStyle
+// @grant        GM_getValue
+// @grant        GM_setValue
+// @grant        GM_xmlhttpRequest
+// @noframes
+// ==/UserScript==
+`
 
 const config: Configuration = {
   entry: "./src/index.ts",
   output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
+    filename: "avotalif.user.js",
+    path: path.resolve(__dirname, "dist")
   },
   module: {
     rules: [
@@ -17,7 +36,19 @@ const config: Configuration = {
   },
   resolve: {
     extensions: [".ts"]
+  },
+  plugins: [
+    new BannerPlugin({
+      banner: scriptHeader,
+      raw: true
+    })
+  ],
+  optimization: {
+    minimize: false
   }
 }
+
+// write meta
+fs.writeFileSync("dist/avotalif.meta.js", scriptHeader)
 
 export default config
